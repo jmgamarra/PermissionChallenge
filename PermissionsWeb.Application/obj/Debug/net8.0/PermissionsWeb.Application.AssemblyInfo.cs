02789e09470faa9c10e190ad8d2f4bd8e6c15600
Jorge Gamarra <jorge.gamarra.ponce@gmail.com>
2024-08-03 19:25:03 -0500

    git ignore

--- PermissionsWeb.Application/obj/Debug/net8.0/PermissionsWeb.Application.AssemblyInfo.cs
+++ PermissionsWeb.Application/obj/Debug/net8.0/PermissionsWeb.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PermissionsWeb.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c225237cd0829aa750801351fe3a1fa411919f64")]
 [assembly: System.Reflection.AssemblyProductAttribute("PermissionsWeb.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PermissionsWeb.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
